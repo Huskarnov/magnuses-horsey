f7ffa225b5cc2cc6d489ffbeb0cd56b09383c295
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-07-13 11:37:18 +0100

    x

--- index.js
+++ index.js
@@ -1,55 +1,85 @@
 class Vertex {
   constructor(x, y) {
     this.adress = [x, y];
-    this.rosace = this.rosaceBuilder();
+    this.rosace = this.#rosaceBuilder();
   }
-  rosaceBuilder() {
-    let rosaceTemp = [];
+  #rosaceBuilder() {
+    let rosaceArray = [];
     if (this.adress[0] + 2 <= 7) {
       if (this.adress[1] + 1 <= 7) {
-        rosaceTemp.push([this.adress[0] + 2, this.adress[1] + 1]);
+        rosaceArray.push([this.adress[0] + 2, this.adress[1] + 1]);
       }
       if (this.adress[1] - 1 >= 0) {
-        rosaceTemp.push([this.adress[0] + 2, this.adress[1] - 1]);
+        rosaceArray.push([this.adress[0] + 2, this.adress[1] - 1]);
       }
     }
     if (this.adress[0] - 2 >= 0) {
       if (this.adress[1] + 1 <= 7) {
-        rosaceTemp.push([this.adress[0] - 2, this.adress[1] + 1]);
+        rosaceArray.push([this.adress[0] - 2, this.adress[1] + 1]);
       }
       if (this.adress[1] - 1 >= 0) {
-        rosaceTemp.push([this.adress[0] - 2, this.adress[1] - 1]);
+        rosaceArray.push([this.adress[0] - 2, this.adress[1] - 1]);
       }
     }
     //
     if (this.adress[1] + 2 <= 7) {
       if (this.adress[0] + 1 <= 7) {
-        rosaceTemp.push([this.adress[0] + 1, this.adress[1] + 2]);
+        rosaceArray.push([this.adress[0] + 1, this.adress[1] + 2]);
       }
       if (this.adress[0] - 1 >= 0) {
-        rosaceTemp.push([this.adress[0] - 1, this.adress[1] + 2]);
+        rosaceArray.push([this.adress[0] - 1, this.adress[1] + 2]);
       }
     }
     if (this.adress[1] - 2 >= 0) {
       if (this.adress[0] + 1 <= 7) {
-        rosaceTemp.push([this.adress[0] + 1, this.adress[1] - 2]);
+        rosaceArray.push([this.adress[0] + 1, this.adress[1] - 2]);
       }
       if (this.adress[0] - 1 >= 0) {
-        rosaceTemp.push([this.adress[0] - 1, this.adress[1] - 2]);
+        rosaceArray.push([this.adress[0] - 1, this.adress[1] - 2]);
       }
     }
 
-    return rosaceTemp;
+    return rosaceArray;
   }
 }
-// class Graph {
-//   constructor(x, y) {
-//     this.root = new Vertex(x, y);
-//     this.visited = [];
-//   }
+class Graph {
+  constructor(x, y) {
+    this.root = [x, y];
+    // this.edgeList = [[], []];
+  }
 
-//   knightMoves(a, b) {}
-// }
+  knightMoves(target) {
+    let queue = [this.root];
+    let visited = [];
+
+    let pointer = 0;
+    // let steps = 0;
+
+    while (queue[pointer]) {
+      if (queue[pointer][0] === target[0] && queue[pointer][1] === target[1]) {
+        console.log("x");
+        return;
+      } else {
+        for (const vertex of new Vertex(queue[pointer][0], queue[pointer][1])
+          .rosace) {
+          if (!visited.includes([vertex[0], vertex[1]])) {
+            visited.push([vertex[0], vertex[1]]);
+
+            // queue.push([vertex[0], vertex[1]]);
+          }
+        }
+        console.log(visited.includes([1, 2]));
+        console.log(visited);
+        console.log(pointer);
+        // queue.shift();
+
+        pointer++;
+      }
+    }
+
+    // rosacebuilder(if exists add to queue BFS)
+  }
+}
 
 // edge list
 // adjacency list
@@ -58,6 +88,6 @@ class Vertex {
 // -+2x -+y
 // -+2y -+x
 
-const vertex = new Vertex(3, 3);
+const graph = new Graph(0, 0);
 
-console.log(vertex.rosace);
+graph.knightMoves([7, 7]);
