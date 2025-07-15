b473d68a651029e465c1404e4ca49fc959b53ada
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-07-15 10:29:56 +0100

    x

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ class Graph {
   constructor(x, y) {
     this.root = [x, y];
     this.edgeList = [];
-    // this.adjacencyList = [[], []];
-    // this.adjacencyMatrix = [[], []];
   }
 
   buildEdgeList() {
@@ -13,19 +11,46 @@ class Graph {
   }
 
   knightMoves(target) {
-    // let queue = [this.root];
+    // early cancel
+    if (target[0] < 0 || target[0] > 7 || target[1] < 0 || target[1] > 7) {
+      alert("target case outside of the table");
+      return;
+    }
+
     let queue = [
       new Vertex(this.root[0], this.root[1], this.root[0], this.root[1]),
     ];
     let tempQueue = [];
-    let visited = queue;
+    let visited = [...queue];
 
     let steps = 0;
 
-    while (steps < 44) {
+    while (true) {
       for (let vertex of queue) {
         if (vertex.adress[0] === target[0] && vertex.adress[1] === target[1]) {
           alert(`target Found in ${steps} steps `);
+
+          let vertexChain = [];
+          let current = structuredClone(vertex);
+          vertexChain.push(current.adress);
+
+          while (
+            current.adress[0] !== current.previous[0] &&
+            current.adress[1] !== current.previous[1]
+          ) {
+            for (let element of visited) {
+              if (
+                element.adress[0] === current.previous[0] &&
+                element.adress[1] === current.previous[1]
+              ) {
+                current = structuredClone(element);
+                vertexChain.push(current.adress);
+                return;
+              }
+            }
+          }
+          console.log(vertexChain);
+
           return;
         }
       }
@@ -38,25 +63,25 @@ class Graph {
 
         for (let rosy of vertexRosace) {
           if (
-            !visited.some(
-              (element) =>
+            !visited.some((element) => {
+              return (
                 element.adress[0] === rosy.adress[0] &&
                 element.adress[1] === rosy.adress[1]
-            )
+              );
+            })
           ) {
-            tempQueue.push(rosy);
             visited.push(rosy);
+            tempQueue.push(rosy);
           }
         }
       }
       steps++;
-      queue = tempQueue;
+      queue = [...tempQueue];
       tempQueue = [];
-      console.log(steps);
     }
   }
 }
 
 const graph = new Graph(0, 0);
 
-graph.knightMoves([3, 3]);
+graph.knightMoves([7, 7]);
